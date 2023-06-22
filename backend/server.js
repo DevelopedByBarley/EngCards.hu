5ab145f4aad8aa9cd5830df71c8ea71ca5b918c2
Árpád Szaniszló <71340344+DevelopedByBarley@users.noreply.github.com>
2023-06-22 13:23:26 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const path = require('path');
 const cors = require('cors');
 const connection = require('./config/database');
 const port = process.env.PORT;
